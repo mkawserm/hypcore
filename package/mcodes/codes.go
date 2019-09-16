22e672b30eca2380dd2492328abc46be204ed490
mkawserm <mkawserm@gmail.com>
2019-09-16 09:05:49 +0600

    CODE REFACTORED

---
+++ package/mcodes/codes.go
@@ -0,0 +1,11 @@
+package mcodes
+
+/*
+Integer 1 - 100 is reserved for Internal Hyper Core Error group
+*/
+
+const AuthGroupCode = 1
+const AuthQueryMustBeUsingPostRequest = 1
+const AuthRequestBodyReadError = 2
+const AuthRequestBodyParseError = 3
+const AuthGraphQLExecutionError = 4
